51e7f63a1537cd43bf6d9989447a1f81067b032d
Grégori Sabel <seriosabel57@gmail.com>
2022-08-02 17:36:12 -0300

    base

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return <Dashboard />
+}
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: '#5636d3',
+
+    secondary: '#ff872c',
+    secondary_light: 'rgba(255,135,44, .3)',
+
+    success: '#12A454',
+    success_light: 'rgba(16,146,84,.5)',
+
+    attenction: '#E83F5B',
+    attenction_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#ffffff',
+    background: '#f0f2f5',
+    text: '#969cb2',
+    title: '#363f5f',
+
+  }
+}
